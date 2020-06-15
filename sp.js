38ae16a85600db1e8efce9ca7a63e2e0bdebfc39
asfand987 <asfand987.ak@gmail.com>
2020-06-15 18:06:45 +0100

    Added delete item functionality.

--- sp.js
+++ sp.js
@@ -1,14 +1,29 @@
 function createList() {
-  var text =  document.getElementById('userInput').value;
-  var listNode = document.getElementById('list');
-  var liNode = document.createElement("Li");
-  var txtNode = document.createTextNode(text);
+  let text =  document.getElementById('userInput').value;
+  let listNode = document.getElementById('list');
+  let liNode = document.createElement("Li");
+  let txtNode = document.createTextNode(text);
+
+  var dBtn = document.createElement("button");
+  dBtn.appendChild(document.createTextNode("X"));
+
+  liNode.append(dBtn);
 
   liNode.appendChild(txtNode);
   listNode.appendChild(liNode);
   clearInput();
+
+  dBtn.onclick = function() {deleteListItem(dBtn)};
+  //dBtn.addEventListener("click", deleteListItem(dBtn));
+
 }
 
 function clearInput() {
     document.getElementById('userInput').value = '';
+}
+
+function deleteListItem(e)
+{
+  console.log("testing");
+  e.parentNode.parentNode.removeChild(e.parentNode)
 }
